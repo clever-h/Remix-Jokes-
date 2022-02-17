70894c8abfdcc7c8186cd47d7a04575562c7c3e5
Hamidreza <Hamid.hrc7@yahoo.com>
2022-02-17 19:39:37 +0330

    fetch data db!

---
+++ app/routes/index.tsx
@@ -0,0 +1,33 @@
+import type { LinksFunction } from "remix";
+import stylesUrl from "~/styles/index.css";
+import { Link } from "remix";
+
+export const links: LinksFunction = () => {
+    return [
+      {
+        rel: "stylesheet",
+        href: stylesUrl
+      }
+    ];
+  };
+
+export default function IndexRoute(){
+    return (
+        <div className="container">
+            <div className="content">
+             <h1>
+                Remix <span>!Jokes</span>
+             </h1>
+             <nav>
+              <ul>
+                <li>
+                <Link to="jokes">
+                    کلیک کن تا جوک بگم!!!
+                </Link>
+                </li>
+             </ul>
+        </nav>
+            </div>
+        </div>
+    )
+}
